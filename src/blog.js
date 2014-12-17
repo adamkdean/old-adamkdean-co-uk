fc9573c20b5fc098a3d29ee15bebde756fd40925
Adam K Dean <adam@amberfish.co.uk>
2014-12-17 19:34:31 +0000

    Changed blog post format

--- src/blog.js
+++ src/blog.js
@@ -1,5 +1,6 @@
 var S = require('string'),
     fs = require('fs'),
+    async = require('async'),
     config = require('./config');
 
 var update = function() {
@@ -15,19 +16,23 @@ var getBlogFiles = function() {
 
     allFiles.forEach(function(element, index, array) {
         if (element.match(/^([0-9]{4})-([0-9]{2})-([0-9]{2})-([a-z-]+)$/i)) {
-            blogFiles.push(element);
+            blogFiles.push(config.post_dir + '/' + element);
         }
     });
 
     return blogFiles;
 }
 
+var readFileAsync = function(filename, callback) {
+    fs.readFile(filename, 'utf8', callback);
+};
+
 var parseBlogFiles = function(filenames) {
 
-    for (let i = 0; i < filenames.length; i++) {
-
-        console.log(filenames[i]);
-    }
+    async.map(filenames, readFileAsync, function(err, results) {
+        console.log('err', err);
+        console.log('results', results);
+    });
 
 };
 
