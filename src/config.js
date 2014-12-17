41fb30ec22642a1b9d7f5aeaa46b88be96dfec6d
Adam K Dean <adam@amberfish.co.uk>
2014-12-17 19:08:54 +0000

    Added pages and port to config

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
 module.exports = {
-    post_dir: '_posts'
+    http_port: process.env.PORT || 8000,
+    page_dir: process.env.PAGE_DIR || '_pages',
+    post_dir: process.env.POST_DIR || '_posts'
 };
