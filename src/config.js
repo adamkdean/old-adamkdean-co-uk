bf196998412f305ee3fd3fda23748e5b410a1dd0
Adam K Dean <adam@amberfish.co.uk>
2014-12-30 20:00:02 +0000

    Added in koa.js and some sample methods

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    LOGGING: 2, // 0 none, 1 standard, 2 debug
     PORT: process.env.PORT || 8000,
     PAGE_DIR: process.env.PAGE_DIR || '_pages',
     POST_DIR: process.env.POST_DIR || '_posts'
--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
-var config = require('./config'),
-    blog = require('./blog');
+var koa = require('koa'),
+    blog = require('./blog'),
+    config = require('./config');
 
-blog.updateAsync(function() {
-    // blog.getPosts();
-    // blog.getPosts({
-    //     slug: 'test-post-one'
-    // });
+var app = koa();
 
-    var posts = blog.getPosts({
-        slug: 'test-post-one'
-    });
+// get the blog started
+blog.startUpdateCycle();
 
-    console.log(posts);
+// x-response-time
+app.use(function *(next){
+    var start = new Date;
+    yield next;
+    var ms = new Date - start;
+    this.set('X-Response-Time', ms + 'ms');
+    if (config.LOGGING >= 2) console.log('X-Response-Time: %s ms', ms);
 });
+
+// logger
+app.use(function *(next){
+    var start = new Date;
+    yield next;
+    var ms = new Date - start;
+    if (config.LOGGING >= 1) console.log('%s %s - %s', this.method, this.url, ms);
+});
+
+// reponse
+app.use(function *(){
+    this.body = 'Hello World';
+});
+
+app.listen(config.PORT);
+console.log('listening on port %d', config.PORT);
