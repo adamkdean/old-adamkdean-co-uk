4cede760eb92c51f0892a8aefc3d59f4be3c3958
Adam Dean <adam.dean@bravissimo.com>
2015-02-10 14:43:11 +0000

    Removed version from the app. Unrequired

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 module.exports = {
     /* you probably don't want to change this */
-    VERSION: '0.1.0',
     PORT: process.env.PORT || 8000,
     CACHE_UPDATE_INTERVAL: process.env.CACHE_UPDATE_INTERVAL || 60000,
     SITE_DIR: process.env.SITE_DIR || '_site',
