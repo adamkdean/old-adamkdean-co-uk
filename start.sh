fbfdd2fd768e0bdc34f85ca31565a44a74090c45
Adam K Dean <adamkdean@googlemail.com>
2015-06-01 23:47:29 +0100

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Builds, configures, and starts my damn site
+
+DOCKER_HOST=tcp://swarm.cloudkeeper.io:12375
+PRIVATE_IPV4=$(ifconfig eth1 | grep "inet addr" | awk -F: '{print $2}' | awk '{print $1}')
+ARRAY=(1 2 3)
+NAME=adamkdean-co-uk
+
+# build it if we don't already have it
+if [ $(docker images | grep ${NAME} | wc -l) -eq 0 ]; then
+    docker build -t $NAME .
+fi
+
+for i in "${ARRAY[@]}"
+do
+    echo "Running ${NAME}_${i}"
+    docker run -d \
+        -p $PRIVATE_IPV4:0:8000 \
+        --name ${NAME}_${i} \
+        --restart=always \
+        $NAME
+done
