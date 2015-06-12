a4c849ad9e5da1b58343a02f26dbf9a7ff352c72
Adam K Dean <adam@amberfish.co.uk>
2015-06-12 09:55:39 +0100

    Disabled run script

--- jenkins/run.sh
+++ jenkins/run.sh
@@ -8,28 +8,28 @@ REGISTRY="registry.cloudkeeper.io"
 NAME="adamkdean-co-uk"
 SCALE=1
 
-echo "[notice] For now, we'll just take down the old containers and start the new ones"
-echo "[notice] Eventually we should make sure the new ones are up before killing the others"
-echo " ----------------------------------------------------------------"
-
-i="0"
-while [ $i -lt $SCALE ]; do
-
-    echo "[info] Pulling $REGISTRY/$NAME:latest..."
-    docker pull $REGISTRY/$NAME:latest
-
-    echo "[info] Killing & removing ${NAME}_${i}..."
-    docker -H $SWARM kill ${NAME}_${i}
-    docker -H $SWARM rm ${NAME}_${i}
-
-    echo "[info] Running ${NAME}_${i}..."
-    docker -H $SWARM run -d -P \
-        -e DEBUG_TIMESTAMP="$(date +'%T')" \
-        --name ${NAME}_${i} \
-        --restart=always \
-        $REGISTRY/$NAME:latest
-
-    echo " ----------------------------------------------------------------"
-
-    i=$[$i+1]
-done
+# echo "[notice] For now, we'll just take down the old containers and start the new ones"
+# echo "[notice] Eventually we should make sure the new ones are up before killing the others"
+# echo " ----------------------------------------------------------------"
+#
+# i="0"
+# while [ $i -lt $SCALE ]; do
+#
+#     echo "[info] Pulling $REGISTRY/$NAME:latest..."
+#     docker pull $REGISTRY/$NAME:latest
+#
+#     echo "[info] Killing & removing ${NAME}_${i}..."
+#     docker -H $SWARM kill ${NAME}_${i}
+#     docker -H $SWARM rm ${NAME}_${i}
+#
+#     echo "[info] Running ${NAME}_${i}..."
+#     docker -H $SWARM run -d -P \
+#         -e DEBUG_TIMESTAMP="$(date +'%T')" \
+#         --name ${NAME}_${i} \
+#         --restart=always \
+#         $REGISTRY/$NAME:latest
+#
+#     echo " ----------------------------------------------------------------"
+#
+#     i=$[$i+1]
+# done
