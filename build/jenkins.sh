f02bf680af1f2753af4491a591e6b177b8f16a95
Adam K Dean <adam@amberfish.co.uk>
2015-06-12 11:59:01 +0100

    Removed line breaks from build script

--- build/jenkins.sh
+++ build/jenkins.sh
@@ -2,7 +2,7 @@
 #
 # Jenkins build script
 
-echo " ---------------------------------------------------------------- "
+
 
 DISCOVERY="consul://swarm.cloudkeeper.io:8500/swarm"
 SWARM="tcp://swarm.cloudkeeper.io:12375"
@@ -16,22 +16,18 @@ docker build -t "build_$NAME" .
 docker tag -f "build_$NAME:latest" "$REGISTRY/$NAME:latest"
 docker rmi "build_$NAME"
 docker push "$REGISTRY/$NAME:latest"
-echo " ---------------------------------------------------------------- "
 
 # Step 2. Get all Swarm nodes to pull new image
 echo "[info] Co-ordinating image update on Swarm nodes..."
-echo " ---------------------------------------------------------------- "
 HOSTS=$(docker run --rm swarm list $DISCOVERY)
 for host in $HOSTS; do
     echo "[info] Updating $REGISTRY/$NAME:latest on $host..."
     docker -H $host pull "$REGISTRY/$NAME:latest"
-    echo " ---------------------------------------------------------------- "
 done
 
 # Step 3. Run the containers
 echo "[notice] For now, we'll just take down the old containers and start the new ones"
 echo "[notice] Eventually we should make sure the new ones are up before killing the others"
-echo " ---------------------------------------------------------------- "
 
 i="0"
 while [ $i -lt $SCALE ]; do
@@ -47,10 +43,7 @@ while [ $i -lt $SCALE ]; do
         --restart=always \
         $REGISTRY/$NAME:latest
 
-    echo " ---------------------------------------------------------------- "
-
     i=$[$i+1]
 done
 
-echo " ---------------------------------------------------------------- "
 echo "Done!"
